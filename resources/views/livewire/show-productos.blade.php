a96cdfa4633b0dedcb98c688acce97bf3c4841a3
Pablo Humeniy <pablo.humeniy@gmail.com>
2022-06-23 02:49:08 -0400

    opcion "agregar producto"

--- resources/views/livewire/show-productos.blade.php
+++ resources/views/livewire/show-productos.blade.php
@@ -1,13 +1,6 @@
 <div>
     <div class="flex relative mt-1">
-        @can('productos.create')
-            <div class="flex-none mr-10">
-                <a href="/productos/create" type="button"
-                    class="focus:outline-none text-white text-sm py-3 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg">
-                    Agregar producto
-                </a>
-            </div>
-        @endcan
+        
         <input type="text"
             class="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500 transition-colors"
             placeholder="Buscar" wire:model="search" type="search">
