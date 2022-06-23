0579494b929ccf122ac40ac777ceac73e4d2dcfd
Pablo Humeniy <pablo.humeniy@gmail.com>
2022-06-23 02:37:46 -0400

    vista productos

--- resources/views/livewire/show-users.blade.php
+++ resources/views/livewire/show-users.blade.php
@@ -49,23 +49,7 @@
                                     {{ $user->estado->descripcionEstado }}
                                 </div>
                             </td>
-                            @can('usuarios.edit')
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="flex item-center justify-center">
-                                        @can('usuarios.edit')
-                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                                <a href="{{ route('usuarios.edit', $user) }}">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                        stroke="currentColor" class="stroke-current text-green-600">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        @endcan
-                                    </div>
-                                </td>
-                            @endcan
+                            
                         </tr>
                     @endforeach
                 </tbody>
