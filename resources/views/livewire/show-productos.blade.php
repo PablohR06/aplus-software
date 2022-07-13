28ac31e2210e64d6dd976737f93b53bf3af2127f
Pablo Humeniy <pablo.humeniy@gmail.com>
2022-07-13 06:27:30 -0400

    agregar formulario y tabla de productos

---
+++ resources/views/livewire/show-productos.blade.php
@@ -0,0 +1,76 @@
+<div>
+    <div class="flex relative mt-1">
+        
+        <input type="text"
+            class="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500 transition-colors"
+            placeholder="Buscar" wire:model="search" type="search">
+    </div>
+    @if ($productos->count() > 0)
+        <x-table>
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead>
+                    <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal font-bold">
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Codigo</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Nombre</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Descripción</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Cantidad</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Precio Compra</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Precio Neto</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Precio + IVA</th>
+                        <th scope="col" class="px-6 py-3 text-left uppercase tracking-wider">Categoria</th>
+                        
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($productos as $producto)
+                        <tr class="hover:bg-gray-100">
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm font-medium text-gray-900">
+                                    {{ $producto->codigo }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                    {{ $producto->nombreProducto }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-normal">
+                                <div class="text-sm text-gray-900">
+                                    {{ $producto->descripcionProducto }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                    {{ $producto->stock }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                    ${{ number_format($producto->precioCompra, 0, ',', '.') }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                    ${{ number_format($producto->precioNeto, 0, ',', '.') }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                    ${{ number_format($producto->precioIva, 0, ',', '.') }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                    {{ $producto->categoria != null ? $producto->categoria->nombreCategoria : 'Sin categoria' }}
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </x-table>
+        {{ $productos->links() }}
+    @else
+        <h2 class="mt-4 font-bold">¡No hay resultados!</h2>
+    @endif
+</div>
